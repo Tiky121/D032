a008eabccde83cab8bc1a62bf2c897c5918f6c6b
Tiky <101576251+Tiky121@users.noreply.github.com>
2025-11-23 13:07:35 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -10,23 +10,48 @@ const peerServer = ExpressPeerServer(server, { debug: true, path: '/' });
 app.use('/peerjs', peerServer);
 app.use(express.static('public'));
 
+// Úložisko stavu
 let activeMonitors = {}; 
 let activeBigScreen = null;
+let activeRemotes = {}; // { socketId: peerId }
 
 io.on('connection', (socket) => {
+    
+    // --- MONITORY ---
     socket.on('register-monitor', (monitorNum, peerId) => {
         activeMonitors[monitorNum] = peerId;
-        console.log(`Monitor ${monitorNum} ID: ${peerId}`);
         io.emit('monitor-available', monitorNum, peerId);
     });
 
     socket.on('get-monitor-id', (monitorNum) => {
         if (activeMonitors[monitorNum]) {
-            // ZMENA: Posielame späť aj číslo monitora!
             socket.emit('monitor-id-response', monitorNum, activeMonitors[monitorNum]);
         }
     });
 
+    // --- REMOTE UŽÍVATELIA (NOVÉ) ---
+    socket.on('register-remote-user', (peerId) => {
+        // Uložíme si nového účastníka
+        activeRemotes[socket.id] = peerId;
+        
+        // 1. Pošleme novému účastníkovi zoznam tých, čo už sú tam
+        const existingUsers = Object.values(activeRemotes).filter(id => id !== peerId);
+        socket.emit('existing-remotes', existingUsers);
+
+        // 2. Povieme ostatným: "Prišiel nový človek!"
+        socket.broadcast.emit('new-remote-joined', peerId);
+    });
+
+    socket.on('disconnect', () => {
+        // Ak odišiel remote user, vymažeme ho a povieme ostatným
+        if (activeRemotes[socket.id]) {
+            const peerId = activeRemotes[socket.id];
+            delete activeRemotes[socket.id];
+            io.emit('remote-disconnected', peerId);
+        }
+    });
+
+    // --- BIG SCREEN & SHARE ---
     socket.on('register-bigscreen', (peerId) => {
         activeBigScreen = peerId;
         io.emit('bigscreen-available', peerId);
